af8074158135e9c2da1568b1db195de279b3b187
Vladimir <shikarno_3000@inbox.ru>
2020-05-04 17:30:06 +0300

    refactoring

--- src/test/java/ApiTest.java
+++ src/test/java/ApiTest.java
@@ -10,9 +10,6 @@ public class ApiTest {
     public void rosElTorgTest() {
         Document page = Steps.goToRosElTorg();
         List<Map<String, Object>> purchases = Steps.getPurchases(page);
-//        System.out.println(purchases.size());
-//        System.out.println("---------");
-//        System.out.println(purchases.stream().filter(element -> Double.parseDouble(element.get("PRICE").toString()) < 4000000.00).count());
 
         purchases.stream()
                 .filter(element -> Double.parseDouble(element.get("PRICE").toString()) < 4000000.00)
